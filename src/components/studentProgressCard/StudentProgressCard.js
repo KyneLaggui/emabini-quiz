7f38fd5f350109f8044d5b4c1d80182c3368b070
Buhain, Joseph Jason S <josephjasonsbuhain@iskolarngbayan.pup.edu.ph>
2023-11-30 09:47:31 +0800

    Added some elements to student progress page.

---
+++ src/components/studentProgressCard/StudentProgressCard.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import './StudentProgressCard.scss';
+import { 
+    Chart as ChartJS,
+    CategoryScale, 
+    LinearScale} from 'chart.js';
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale
+)
+
+const StudentProgressCard = ({ courseTitle, courseCode }) => {
+    const data = {
+        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+        datasets: [
+          {
+            label: "First dataset",
+            data: [33, 53, 85, 41, 44, 65],
+            fill: true,
+            backgroundColor: "rgba(75,192,192,0.2)",
+            borderColor: "rgba(75,192,192,1)"
+          },
+          {
+            label: "Second dataset",
+            data: [33, 25, 35, 51, 54, 76],
+            fill: false,
+            borderColor: "#742774"
+          }
+        ]
+      };
+
+    return (
+        <div className="progress-card-wrapper">
+            <div className="progress-left-details">
+                <div>
+                    <p className="bold medium-text">{courseTitle}</p>
+                    <p>{courseCode}</p>
+                </div>
+                <div className="progress-card-percentages"> 
+                    <p>Introduction to Operating Systems: 75%</p>
+                    <p>WSL: 80%</p>
+                    <p>Terminal: 25%</p>
+                    <p>CPU Scheduling: 75%</p>
+                </div>            
+            </div>
+            <div>
+                <Line data={data} />
+            </div>
+        </div>
+    )
+}
+
+export default StudentProgressCard
---
+++ src/pages/StudentProgress/StudentProgress.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import './StudentProgress.scss';
+import StudentProgressCard from '../../components/studentProgressCard/StudentProgressCard';
+import './StudentProgress.scss';
+import Sidebar from '../../components/Sidebar/Sidebar';
+import PageLayout from '../../layouts/pageLayout/PageLayout';
+
+const StudentProgress = () => {
+    const courses = [
+        {
+            courseCode: 'CMPE 10113',
+            courseTitle: 'Operating Systems'
+        },
+        {
+            courseCode: 'CMPE 40062',
+            courseTitle: 'Web Development'
+        },
+        {
+            courseCode: 'CMPE 30113',
+            courseTitle: 'Software Design'
+        },
+        {
+            courseCode: 'CMPE 30043',
+            courseTitle: 'Discrete Mathematics'
+        },
+        {
+            courseCode: 'MATH 20053',
+            courseTitle: 'Calculus 2'
+        },
+        {
+            courseCode: 'PHED 10022',
+            courseTitle: 'Rhythmic Activities'
+        },
+    ]
+  
+  return (
+    <>  
+        <Sidebar />
+        <PageLayout>
+            <div className="student-progress-wrapper">
+                {courses.length === 0 ? (
+                        <p>No courses found.</p>
+                        ) : (
+                            
+                            courses.map((course, i) => {
+                                return (
+                                    <StudentProgressCard {...course} key={i}/>                            
+                                )
+                            })
+                        )
+                    
+                }
+            </div>            
+        </PageLayout>
+    </>
+  )
+}
+
+export default StudentProgress
