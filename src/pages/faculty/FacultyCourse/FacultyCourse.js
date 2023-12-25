e0911c3c9f1cf43704ad327d8eb2d66aa6103a06
KayDeev/ <92616644+KyneLaggui@users.noreply.github.com>
2023-12-25 23:44:55 +0800

    Overflow issue within Card, Added some styling to Settings Dropdown

--- src/pages/faculty/FacultyCourse/FacultyCourse.js
+++ src/pages/faculty/FacultyCourse/FacultyCourse.js
@@ -1,8 +1,66 @@
 import React from 'react'
+import Sidebar from '../../../components/Sidebar/Sidebar'
+import PageLayout from '../../../layouts/pageLayout/PageLayout'
+import "./FacultyCourse.scss"
+import CourseFacultyCard from '../../../components/courseRelated/courseFacultyCard/CourseFacultyCard'
 
 const FacultyCourse = () => {
+    const courses = [
+        {
+            courseCode: 'CMPE 10113',
+            courseTitle: 'Operating Systems',
+            courseStudents : '24'
+        },
+        {
+            courseCode: 'CMPE 40062',
+            courseTitle: 'Web Development',
+            courseStudents : '24'
+        },
+        {
+            courseCode: 'CMPE 30113',
+            courseTitle: 'Software Design',
+            courseStudents : '24'
+        },
+        {
+            courseCode: 'CMPE 30043',
+            courseTitle: 'Discrete Mathematics',
+            courseStudents : '24'
+        },
+        {
+            courseCode: 'MATH 20053',
+            courseTitle: 'Calculus 2',
+            courseStudents : '24'
+        },
+        {
+            courseCode: 'PHED 10022',
+            courseTitle: 'Rhythmic Activities',
+            courseStudents : '24'
+        },
+    
+    ]
+    
   return (
-    <div>FacultyCourse</div>
+    <>
+        <Sidebar></Sidebar>
+        <PageLayout>
+        
+            <div className='courses-orie'>
+                {courses.length === 0 ? (
+                    <p>No courses found.</p>
+                    ) : (
+                        
+                        courses.map((course, i) => {
+                            return (
+                                <CourseFacultyCard {...course} key={i}/>
+                                
+                            )
+                        })
+                    )
+                
+                }
+            </div>
+        </PageLayout>
+    </>
   )
 }
 
