f187be14230cde24200005f403a56f54cb440432
KayDeev/ <92616644+KyneLaggui@users.noreply.github.com>
2023-12-27 16:04:05 +0800

    Added Modal Implementation without Contents

--- src/components/courseRelated/facultyStudentsList/FacultyStudentsList.js
+++ src/components/courseRelated/facultyStudentsList/FacultyStudentsList.js
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./FacultyStudentsList.scss"
 import { IoRemoveCircle } from 'react-icons/io5';
+import Modal from 'react-modal';
 
+Modal.setAppElement('#root');
 const FacultyStudentsList = ({ dynamicHeight }) => {
+    
+    const [modalIsOpen, setIsOpen] = useState(false);
+
+    const customStyles = {
+        content: {
+          top: '50%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)',
+          background: '#F3F6FF',
+          borderRadius: '5px',
+          width: '100%',
+          maxWidth: '500px',
+          padding: '40px 40px 20px 40px'
+        },
+        
+      };
+
+    function openModal() {
+        setIsOpen(true);
+      }
+    
+    //   function afterOpenModal() {
+    
+    //     subtitle.style.color = '#f00';
+    //   }
+    
+      function closeModal() {
+        setIsOpen(false);
+      }
+
     const studentNames = [
         'Jason Buhamid',
         'Jason Downbad',
@@ -27,7 +62,7 @@ const FacultyStudentsList = ({ dynamicHeight }) => {
     <div className='fsl-container' style={{ height: dynamicHeight }}>
         <div className='fsl-title-wrapper'>
             <h1>Students</h1>
-            <p>View</p>
+            <p onClick={openModal}>View</p>
         </div>
         <div className='fsl-students-wrapper'>
                 {studentNames.map((student, index) => (
@@ -37,7 +72,18 @@ const FacultyStudentsList = ({ dynamicHeight }) => {
                     </div>
                 ))}
         </div>
-        
+        <div>       
+            <Modal
+                isOpen={modalIsOpen}
+                // onAfterOpen={afterOpenModal}
+                onRequestClose={closeModal}
+                style={customStyles}
+                contentLabel="Registration Modal"
+            >   
+                
+            </Modal>
+        </div>
+      
     </div>
     
     
