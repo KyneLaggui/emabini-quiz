e8635cbe5f2ae69ec837ccb00e15428f02b5fc5b
KayDeev/ <92616644+KyneLaggui@users.noreply.github.com>
2024-01-01 22:42:44 +0800

    Created a File for Multiple Choice Quiz Creation Tab

---
+++ src/pages/faculty/CreateQuiz/CreateMultipleChoice.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./CreateMultipleChoice.scss"
+
+const CreateMultipleChoice = () => {
+  return (
+    <div>CreateMultipleChoice</div>
+  )
+}
+
+export default CreateMultipleChoice
