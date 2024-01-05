68fedc5a63f954c6697c6a647a209f182b729072
Buhain, Joseph Jason S <josephjasonsbuhain@iskolarngbayan.pup.edu.ph>
2024-01-05 22:36:50 +0800

    Making account management table dynamic and announcement creation.

--- src/components/courseRelated/courseAnnouncements/CourseAnnouncements.js
+++ src/components/courseRelated/courseAnnouncements/CourseAnnouncements.js
@@ -1,63 +1,75 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./CourseAnnouncements.scss"
 import Modal from 'react-modal';
 import { CiCircleMore } from "react-icons/ci";
 import Multiselect from 'multiselect-react-dropdown';
+import { supabase } from "../../../supabase/config";
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
+import { IoCalendarOutline } from 'react-icons/io5';
+import { AiOutlineSync } from 'react-icons/ai';
+import { Link } from "react-router-dom";
 
 Modal.setAppElement('#root');
 
-export const CourseAnnouncements = () => {
-    const announcements = [
-        {
-            announcementTitle : 'CPU Scheduling Examination',
-            announcementDate : 'November 16, 2023',
-            announcementContent: 'Get ready for the examination of the CPU Scheduling Examination',
-        },
-        {
-            announcementTitle : 'CPU Scheduling Examination',
-            announcementDate : 'November 16, 2023',
-            announcementContent: 'Get ready for the examination of the CPU Scheduling Examination',
-        },
-        {
-            announcementTitle : 'Midterm Examination',
-            announcementDate : 'November 23, 2023',
-            announcementContent: 'Only use pantindahan calculator',
-        },
-        {
-            announcementTitle : 'Midterm Examination',
-            announcementDate : 'November 23, 2023',
-            announcementContent: 'Only use pantindahan calculator',
-        },
-        {
-            announcementTitle : 'Midterm Examination',
-            announcementDate : 'November 23, 2023',
-            announcementContent: 'Get ready for the examination of the CPU Scheduling ExaminationGet ready for the examination of the CPU Scheduling ExaminationGet ready for the examination of the CPU Scheduling ExaminationGet ready for the examination of the CPU Scheduling Examination',
-        },
-        
+export const CourseAnnouncements = ({ courseCode, code, name }) => {
     
-    ]
+    const [announcements, setAnnouncements] = useState([])
+    const [formData, setFormData] = useState({
+        title: '',
+        course: code,
+        content: ''
+    })
+
+    const [scheduleDate, setScheduleDate] = useState(new Date());
+    const [showDatePicker, setShowDatePicker] = useState(false);
+  
+    const changeDateHandler = (e) => {
+        e.preventDefault();
+        setShowDatePicker(!showDatePicker);
+    }
+
+    // const announcements = [
+    //     {
+    //         announcementTitle : 'CPU Scheduling Examination',
+    //         announcementDate : 'November 16, 2023',
+    //         announcementContent: 'Get ready for the examination of the CPU Scheduling Examination',
+    //     },
+    //     {
+    //         announcementTitle : 'CPU Scheduling Examination',
+    //         announcementDate : 'November 16, 2023',
+    //         announcementContent: 'Get ready for the examination of the CPU Scheduling Examination',
+    //     },
+    //     {
+    //         announcementTitle : 'Midterm Examination',
+    //         announcementDate : 'November 23, 2023',
+    //         announcementContent: 'Only use pantindahan calculator',
+    //     },
+    //     {
+    //         announcementTitle : 'Midterm Examination',
+    //         announcementDate : 'November 23, 2023',
+    //         announcementContent: 'Only use pantindahan calculator',
+    //     },
+    //     {
+    //         announcementTitle : 'Midterm Examination',
+    //         announcementDate : 'November 23, 2023',
+    //         announcementContent: 'Get ready for the examination of the CPU Scheduling ExaminationGet ready for the examination of the CPU Scheduling ExaminationGet ready for the examination of the CPU Scheduling ExaminationGet ready for the examination of the CPU Scheduling Examination',
+    //     },            
+    // ]
 
     const courses = [
-        { id: 1, name: 'Operating System' },
-        { id: 2, name: 'Differential Equations' },
-        { id: 3, name: 'Logic Circuits and Design' },
-        
-      ];
+        { id: 1, name: courseCode },   
+    ];
 
     const selectStyles = {
         multiselectContainer: {
             padding: "0.5rem",
             background: "white",
-            "min-width" : "500px"
-            
-            
+            "min-width" : "500px"                        
         },
         searchBox: {
             border: "none",
             color: "black",
-            
-
-            
         },
         chips: { 
             background: "var(--redLO)"
@@ -67,13 +79,10 @@ export const CourseAnnouncements = () => {
             color: 'var(--blue)',
             
         },
-    }
-      
+    }      
 
     const [modalIsOpen, setIsOpen] = useState(false);
     const [announcementView, setAnnouncementView] = useState(true);
-
-    
 
     const customStyles = {
       content: {
@@ -96,11 +105,6 @@ export const CourseAnnouncements = () => {
       setIsOpen(true);
     }
   
-  //   function afterOpenModal() {
-  
-  //     subtitle.style.color = '#f00';
-  //   }
-  
     function closeModal() {
       setIsOpen(false);
     }
@@ -108,6 +112,49 @@ export const CourseAnnouncements = () => {
     const toggleView = () => {
       setAnnouncementView(!announcementView); 
     };
+
+    // Form functions
+    const onInputHandleChange = (event) => {
+        const {name, value} = event.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        })            
+        
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const insertAnnouncement = async() => {
+            const { error } = await supabase
+            .from('course_announcement')
+            .insert({
+                title: formData.title,
+                course_code: formData.course,
+                content: formData.content,
+                date: scheduleDate
+            })
+        }
+
+        insertAnnouncement();        
+    }
+
+    const ExampleCustomTimeInput = ({ date, value, onChange }) => (
+        <input
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+          style={{ border: "solid 1px pink" }}
+        />
+      );
+
+    // useEffect(() => {
+        
+    //     setCourse([
+
+    //     ])
+    // }, []) 
+
   return (
     <div className='ca-container'>
         <h1>Announcements</h1>
@@ -124,9 +171,9 @@ export const CourseAnnouncements = () => {
                         </div>
                     )
                 })
-            )
-        
+            )        
         }
+
         <div className="ca-modal">
             <p onClick={openModal}>See all Announcements</p>
         </div>
@@ -141,8 +188,8 @@ export const CourseAnnouncements = () => {
             <div className='modal-main-cont'>
                 <div className='modal-top'>
                     <div className='modal-top-left'>
-                        <h1>Operating Systems</h1>
-                        <p>CMPE 30113</p>
+                        <h1>{name}</h1>
+                        <p>{code}</p>
                     </div>
                     <div className="modal-toggle">
                         <button
@@ -159,9 +206,7 @@ export const CourseAnnouncements = () => {
                         </button>
                     </div>
                 </div>
-
-                
-                
+                                
                 <div className={`modal-details ${announcementView  ? 'enroll-view' : 'existing-view'}`}>
                     {announcementView ? 
                         <>
@@ -206,38 +251,51 @@ export const CourseAnnouncements = () => {
                     : 
                         <>
                             
-                            <div className='ma-inputs'>
-                                <div className='modal-announcement-input'>
-                                    <h1>Announcement Title:</h1>
-                                    <input type='text' placeholder='Enter Course Name...' />
+                            <form className='ma-inputs' onSubmit={handleSubmit}>
+                                <div className='modal-announcement-input title-date-container'>
+                                    <div>
+                                        <h1>Announcement Title:</h1>
+                                        <input type='text' placeholder='Enter Announcement Title...' name="title" onChange={(e) => onInputHandleChange(e)}/>
+                                    </div>
+                                    <div className="date-icon">
+                                        <IoCalendarOutline size={40}  to="/" onClick={(e) => changeDateHandler(e)}/>
+                                        <DatePicker 
+                                            selected={scheduleDate} 
+                                            onChange={(date) => { setScheduleDate(date); setShowDatePicker(false) }} 
+                                            open={showDatePicker} 
+                                            className="date-picker" 
+                                            popperPlacement="bottom"
+
+                                        />
+                                    </div>                                    
                                 </div>
                                 <div className='modal-announcement-input'>
                                     <h1>Assign to:</h1>
                                     <Multiselect
-                                    options={courses} 
+                                    options={courses}
+                                    selectedValues={[courses[0]]}
                                     displayValue="name" 
                                     placeholder="Select Courses"
                                     style= {selectStyles}
+                                    singleSelect={true}
+                                    disable={true}
+                                    name="course"
                                     />
                                 </div>
 
                                 <div className='modal-announcement-input'>
                                     <h1>Announcement Content:</h1>
-                                    <textarea type='text' placeholder='Enter Content...' />
-                                </div>
+                                    <textarea type='text' placeholder='Enter Content...' name="content" onChange={(e) => onInputHandleChange(e)}/>
+                                </div>                    
+                                   
                                 <div className='ca-confirmation'>
                                     <button className='ca-cancel' onClick={closeModal}>Cancel</button>
-                                    <button className='ca-save' >Confirm</button>
-                                </div>
-                                
-                                
-                            </div>
-                            
+                                    <button className='ca-save' type="submit">Confirm</button>
+                                </div>                                                                
+                            </form>                            
                         </>
                     }
-                </div>
-                    
-                
+                </div>                                    
             </div>
                 
             </Modal>
